a5206f1c69e560382bbf55575918d182e9e2ff8b
hoekthemad <s.swift.12.35@gmail.com>
2026-03-23 20:52:19 +0000

    Add function to general utils

--- src/utils/general.php
+++ src/utils/general.php
@@ -26,3 +26,18 @@ function doLogout() {
     header("Location: login.php");
     exit;
 }
+
+/**
+ * Get the current page name from the action
+ * 
+ * @author Hoek
+ * @since Mar 22 2026
+ * @revisions 0
+ */
+function getPageName() {
+    $pName = "Logout";
+    if (!empty($_REQUEST['action'])) {
+        $pName = ucfirst($_REQUEST['action']);
+    }
+    return $pName;
+}
