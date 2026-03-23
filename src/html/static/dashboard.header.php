5bdc7b3e5a6461aabca64d299c3c39d4ad80d599
hoekthemad <s.swift.12.35@gmail.com>
2026-03-23 20:52:06 +0000

    Add icon resources

--- src/html/static/dashboard.header.php
+++ src/html/static/dashboard.header.php
@@ -6,6 +6,7 @@
         <title>Example<?= !empty($currPage) ? " - {$currPage}" : ""; ?></title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-4.0.0.js" crossorigin="anonymous"></script>
         <script>
             jq = $.noConflict();
