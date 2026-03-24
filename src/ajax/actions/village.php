1b35ddd75e036da6e57404f28e913371dbdfef83
AWanweirdLife <140264183+AWanweirdLife@users.noreply.github.com>
2026-03-24 11:15:43 -0600

    Update Village Upgrade Buttons
    Changed upgrade buttons to upgrade modals.

--- src/ajax/actions/village.php
+++ src/ajax/actions/village.php
@@ -1,10 +1,4 @@
 <?php
 
-    $getUserVillage = $connection->prepare(
-        "SELECT * FROM user_village WHERE UserID = $_SESSION['uid']"
-    );
-    $getUserVillage->execute();
-    $result = $getUserVillage->get_result();
-    if ($result->num_rows >= 1) {
-        $row = $result->fetch_array();
-    }
+    $output['success'] = true;
+    $output['response_output'] = $processedData;
--- src/html/actions/village.php
+++ src/html/actions/village.php
@@ -10,7 +10,24 @@
                     <h6 class="card-subtitle mb-2 text-muted">Main Building</h6>
                     <p class="card-text">This building determines the max level for all other buildings.</p>
                     <p class="card-text">Level: 1</p>
-                    <a href="#" class="card-link">Upgrade</a>
+                    <a class="card-link" data-bs-toggle="modal" data-bs-target="#townHallModal" href="#">Upgrade</a>
+                    <div class="modal fade" id="townHallModal" tabindex="-1" aria-labelledby="townHallModal" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="townHallModal">Upgrade Town Hall</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Cost to Upgrade: 0 Coins
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <br>
@@ -21,7 +38,26 @@
                         <h6 class="card-subtitle mb-2 text-muted">Support Building</h6>
                         <p class="card-text">This building determines the speed at which your heroes heal.</p>
                         <p class="card-text">Level: 0</p>
-                        <a href="#" class="card-link">Upgrade</a>
+                        <p class="card-text">Bonus: 0%</p>
+                        <br>
+                        <a class="card-link" data-bs-toggle="modal" data-bs-target="#hospitalModal" href="#">Upgrade</a>
+                        <div class="modal fade" id="hospitalModal" tabindex="-1" aria-labelledby="hospitalModal" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="hospitalModal">Upgrade Hospital</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Cost to Upgrade: 0 Coins
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -34,10 +70,93 @@
                     <p class="card-text">These buildings help generate passive gold income.</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Factory 1 Level: 0<br>0 Gold per minute.<br><a href="#" class="card-link">Upgrade</a></li>
-                    <li class="list-group-item">Factory 2 Level: 0<br>0 Gold per minute.<br><a href="#" class="card-link">Upgrade</a></li>
-                    <li class="list-group-item">Factory 3 Level: 0<br>0 Gold per minute.<br><a href="#" class="card-link">Upgrade</a></li>
-                    <li class="list-group-item">Factory 4 Level: 0<br>0 Gold per minute.<br><a href="#" class="card-link">Upgrade</a></li>
+                    <li class="list-group-item">Factory 1 Level: 0
+                        <br>0 Gold per minute.
+                        <br>
+                        <a class="card-link" data-bs-toggle="modal" data-bs-target="#goldFactory1Modal" href="#">Upgrade</a>
+                        <div class="modal fade" id="goldFactory1Modal" tabindex="-1" aria-labelledby="goldFactory1ModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="goldFactory1ModalLabel">Upgrade Gold Factory 1</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Cost to Upgrade: 0 Coins
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <li class="list-group-item">Factory 2 Level: 0
+                        <br>0 Gold per minute.
+                        <br>
+                        <a class="card-link" data-bs-toggle="modal" data-bs-target="#goldFactory2Modal" href="#">Upgrade</a>
+                        <div class="modal fade" id="goldFactory2Modal" tabindex="-1" aria-labelledby="goldFactory2Modal" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="goldFactory2Modal">Upgrade Gold Factory 2</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Cost to Upgrade: 0 Coins
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">Factory 3 Level: 0
+                        <br>0 Gold per minute.
+                        <br>
+                        <a class="card-link" data-bs-toggle="modal" data-bs-target="#goldFactory3ModalLabel" href="#">Upgrade</a>
+                        <div class="modal fade" id="goldFactory3ModalLabel" tabindex="-1" aria-labelledby="goldFactory3ModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="goldFactory3ModalLabel">Upgrade Gold Factory 3</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Cost to Upgrade: 0 Coins
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">Factory 4 Level: 0
+                        <br>0 Gold per minute.
+                        <br>
+                        <a class="card-link" data-bs-toggle="modal" data-bs-target="#goldFactory4Modal" href="#">Upgrade</a>
+                        <div class="modal fade" id="goldFactory4Modal" tabindex="-1" aria-labelledby="goldFactory4Modal" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="goldFactory4Modal">Upgrade Gold Factory 4</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Cost to Upgrade: 0 Coins
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
                 <div class="card-body">
                     <a href="#" class="card-link">Claim 0 Gold</a>
@@ -52,8 +171,50 @@
                     <p class="card-text">These buildings help generate passive gem income.</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Factory 1 Level: 0<br>0 Gems per minute.<br><a href="#" class="card-link">Upgrade</a></li>
-                    <li class="list-group-item">Factory 2 Level: 0<br>0 Gems per minute.<br><a href="#" class="card-link">Upgrade</a></li>
+                    <li class="list-group-item">Factory 1 Level: 0
+                        <br>0 Gems per minute.
+                        <br>
+                        <a class="card-link" data-bs-toggle="modal" data-bs-target="#gemFactory1Modal" href="#">Upgrade</a>
+                        <div class="modal fade" id="gemFactory1Modal" tabindex="-1" aria-labelledby="gemFactory1Modal" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="gemFactory1Modal">Upgrade Gem Factory 1</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Cost to Upgrade: 0 Coins
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">Factory 2 Level: 0
+                        <br>0 Gems per minute.
+                        <br>
+                        <a class="card-link" data-bs-toggle="modal" data-bs-target="#gemFactory2Modal" href="#">Upgrade</a>
+                        <div class="modal fade" id="gemFactory2Modal" tabindex="-1" aria-labelledby="gemFactory2Modal" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="gemFactory2Modal">Upgrade Gem Factory 2</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Cost to Upgrade: 0 Coins
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal">Upgrade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
                 <div class="card-body">
                     <a href="#" class="card-link">Claim 0 Gems</a>
