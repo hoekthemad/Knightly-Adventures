6fa13e454c02f702fa17872ba05c2c7a9ce8d9d5
AWanweirdLife <140264183+AWanweirdLife@users.noreply.github.com>
2026-03-24 13:38:05 -0600

    Update village.php

--- src/utils/actions/village.php
+++ src/utils/actions/village.php
@@ -1,11 +1,8 @@
 <?php
-/*
-    $getUserVillage = $connection->prepare(
-        "SELECT * FROM user_village WHERE UserID = $_SESSION['uid']"
-    );
+
+    $getUserVillage = $connection->prepare("SELECT * FROM user_village WHERE UserID = $_SESSION['uid']");
     $getUserVillage->execute();
     $result = $getUserVillage->get_result();
     if ($result->num_rows >= 1) {
         $row = $result->fetch_array();
     }
-        */
